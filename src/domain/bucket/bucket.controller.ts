714c120fcadf86e0613b0e9d17f452a9bff69719
JooYoung2274 <jooooo2274@gmail.com>
2023-11-07 09:20:46 +0900

    feature : bucket 기초 crud && module 세팅 추가

---
+++ src/domain/bucket/bucket.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { BucketService } from './bucket.service';
+import { AuthGuard } from '@nestjs/passport';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { User } from 'src/common/decorator/user.decorator';
+
+@Controller('bucket')
+export class BucketController {
+  constructor(private bucketService: BucketService) {}
+
+  @ApiOperation({ summary: '장바구니 등록하기' })
+  @ApiBearerAuth('access-token')
+  @Post()
+  @UseGuards(AuthGuard('jwt'))
+  async createBucket(@Body() body, @User() user) {
+    return await this.bucketService.createBucket(body, user);
+  }
+
+  @ApiOperation({ summary: '장바구니 불러오기' })
+  @ApiBearerAuth('access-token')
+  @Get()
+  @UseGuards(AuthGuard('jwt'))
+  async getBucket(@User() user) {
+    return await this.bucketService.getBucket(user);
+  }
+
+  @ApiOperation({ summary: '장바구니 삭제' })
+  @ApiBearerAuth('access-token')
+  @Delete()
+  @UseGuards(AuthGuard('jwt'))
+  async deleteBucket(@Param() param, @User() user) {
+    return await this.bucketService.deleteBucket(param, user);
+  }
+
+  @ApiOperation({ summary: '장바구니 알람주기 변경' })
+  @ApiBearerAuth('access-token')
+  @Put()
+  @UseGuards(AuthGuard('jwt'))
+  async updateBucket(@Body() body, @Param() param, @User() user) {
+    return await this.bucketService.updateBucket(body, param, user);
+  }
+}
---
+++ src/domain/bucket/bucket.service.ts
@@ -0,0 +1,64 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Bucket } from '../entities/Bucket';
+import { Repository } from 'typeorm';
+import { Items } from '../entities/item';
+
+@Injectable()
+export class BucketService {
+  constructor(
+    @InjectRepository(Bucket) private bucketRepository: Repository<Bucket>,
+    @InjectRepository(Items) private itemRepository: Repository<Items>,
+  ) {}
+
+  async createBucket(body, user) {
+    const { ItemId, cycle } = body;
+    const { id } = user;
+
+    const newBucket = this.bucketRepository.create();
+    newBucket.UserId = id;
+    newBucket.cycle = cycle;
+    newBucket.ItemId = ItemId;
+    return await this.bucketRepository.save(newBucket);
+  }
+
+  async getBucket(user) {
+    const { id } = user;
+
+    return await this.itemRepository
+      .createQueryBuilder('i')
+      .innerJoin('i.Bucket', 'b')
+      .where('b.UserId =:id', { id })
+      .getRawMany();
+  }
+
+  async deleteBucket(param, user) {
+    const { BucketId } = param;
+    const { id } = user;
+
+    return await this.bucketRepository.delete({
+      id: Number(BucketId),
+      UserId: id,
+    });
+  }
+
+  async updateBucket(body, param, user) {
+    const { cycle } = body;
+    const { BucketId } = param;
+    const { id } = user;
+
+    const isSubscribe = await this.bucketRepository.findOne({
+      where: {
+        id: Number(BucketId),
+        UserId: id,
+      },
+    });
+
+    if (isSubscribe.UserId !== id) {
+      throw new BadRequestException();
+    }
+
+    isSubscribe.cycle = cycle;
+    return await this.bucketRepository.save(isSubscribe);
+  }
+}
